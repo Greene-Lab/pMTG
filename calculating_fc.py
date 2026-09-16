a45f6ce70b89b4b24e133503f817cfc3be4a1898
Emily Koithan <emkoithan@ucsd.edu>
2026-09-16 11:28:46 -0500

    Added code to allow for use of full network templates in addition to probabilistic

--- calculating_fc.py
+++ calculating_fc.py
@@ -26,9 +26,12 @@ surface_files = {
 # Input variant density and network maps, motion summary file, and subject-level derivative directory.
 density_path = '/labs/greene-lab/lab_members/emily/mtg/maps/Variants_Density_cPFM_not06not11not13_185avrg.dtseries.nii'
 avg_path = '/labs/greene-lab/lab_members/emily/mtg/maps/abcd_template_matching_combined_clusters_thresh0.61.dlabel.nii'
+avg_path_full = '/labs/greene-lab/Shared_Tools/Group_Averages/ABCD_Combined_DISCandREP/ABCD_COMBINED_island_getaway_correction_with_SCAN.dlabel.nii'
 motion_path = '/labs/greene-lab/lab_members/emily/mtg/data/motion_data_summary.csv'
 base_dir = '/labs/greene-lab/ABCD/derivatives/GL_Smoothing/'
 subject_dirs = glob(os.path.join(base_dir, "sub-*"))
+
+FULL_NETWORKS = True # If True, uses avg_oath_full rather than avg_path
 
 # Output maps for the thresholded variant clusters and pMTG masks.
 cluster_output_path = '/labs/greene-lab/lab_members/emily/mtg/maps/Variants_Density_cPFM_not06not11not13_185avrg_thresholded_clusters.dtseries.nii'
@@ -73,67 +76,124 @@ nib.save(mtg_img, mtg_output_path)
 print("MTG vertices saved to:", mtg_output_path)
 
 # Step 3: Load average network map and compute group-average network time courses (excluding MTG vertices)
-avg_img = nib.load(avg_path)
-avg_data = avg_img.get_fdata().squeeze()
+if not FULL_NETWORKS:
+    avg_img = nib.load(avg_path)
+    avg_data = avg_img.get_fdata().squeeze()
 
-print('Shape of group average:', avg_data.shape)
-print('Unique numeric labels in average map:', np.unique(avg_data))
-# Define network labels and their corresponding numeric labels from the MIDB probabilistic network template
-# Only cortical regions are included, and pMTG vertices are excluded from the network masks to avoid self-correlation
-network_labels = {'DMN_left': [1, 16, 18, 28, 30, 73, 74], 
-                  'DMN_right': [33, 49, 58, 60, 62, 81, 84], 
-                  'SMl_left': [2, 79, 96, 104], 
-                  'SMl_right': [34, 90, 100, 106], 
-                  'Vis_left': [3, 77, 91, 103], 
-                  'Vis_right': [35, 89, 92, 105], 
-                  'FP_left': [4, 21, 27, 29, 71, 76], 
-                  'FP_right': [36, 37, 53, 59, 61, 82, 86, 87], 
-                  'Aud_left': [5, 98], 
-                  'Aud_right': [38, 102], 
-                  'Tpole_left': [6, 64], 
-                  'Tpole_right': [39, 65], 
-                  'PMN_left': [7, 24], 
-                  'PMN_right': [40, 55], 
-                  'PON_left': [8, 26], 
-                  'PON_right': [41, 57], 
-                  'MTL_left': [9, 93], 
-                  'MTL_right': [42, 94], 
-                  'CO_left': [10, 17, 20, 32, 69, 75, 97], 
-                  'CO_right': [43, 48, 51, 63, 101, 107], 
-                  'Sal_left': [11, 23, 31, 66, 95], 
-                  'Sal_right': [44, 54, 67, 99], 
-                  'SMd_left': [12, 13, 68, 78], 
-                  'SMd_right': [45, 80, 88], 
-                  'DAN_left': [14, 15, 25, 70, 72], 
-                  'DAN_right': [46, 47, 56, 83, 85], 
-                  'VAN_left': [19, 22], 
-                  'VAN_right': [50, 52],
-                  'DMN_full': [1, 16, 18, 28, 30, 33, 49, 58, 60, 62, 73, 74, 81, 84], 
-                  'SMl_full': [2, 34, 79, 90, 96, 100, 104, 106], 
-                  'Vis_full': [3, 35, 77, 89, 91, 92, 103, 105], 
-                  'FP_full': [4, 21, 27, 29, 36, 37, 53, 59, 61, 71, 76, 82, 86, 87], 
-                  'Aud_full': [5, 38, 98, 102], 
-                  'Tpole_full': [6, 39, 64, 65], 
-                  'PMN_full': [7, 24, 40, 55], 
-                  'PON_full': [8, 26, 41, 57], 
-                  'MTL_full': [9, 42, 93, 94], 
-                  'CO_full': [10, 17, 20, 32, 43, 48, 51, 63, 69, 75, 97, 101, 107], 
-                  'Sal_full': [11, 23, 31, 44, 54, 66, 67, 95, 99], 
-                  'SMd_full': [12, 13, 45, 68, 78, 80, 88], 
-                  'DAN_full': [14, 15, 25, 46, 47, 56, 70, 72, 83, 85], 
-                  'VAN_full': [19, 22, 50, 52]}
-print('Defined network labels and their numeric labels.')
+    print('Shape of group average:', avg_data.shape)
+    print('Unique numeric labels in average map:', np.unique(avg_data))
+    # Define network labels and their corresponding numeric labels from the MIDB probabilistic network template
+    # Only cortical regions are included, and pMTG vertices are excluded from the network masks to avoid self-correlation
 
-# Convert each network's numeric labels into vertex indices and exclude pMTG vertices if there is any overlap
-# Overlap should only be a few right pMTG vertices bordering the temporal VAN region
-network_indices = {}
-for network, labels in network_labels.items():
-    indices = np.where(np.isin(avg_data, labels))[0]
-    # remove any MTG indices from the network mask
-    indices = list(set(indices) - set(mtg_list))
-    network_indices[network] = indices
-    print(f'Network {network} has {len(indices)} vertices.')
-print('Network indices computed.')
+    network_labels = {'DMN_left': [1, 16, 18, 28, 30, 73, 74], 
+                    'DMN_right': [33, 49, 58, 60, 62, 81, 84], 
+                    'SMl_left': [2, 79, 96, 104], 
+                    'SMl_right': [34, 90, 100, 106], 
+                    'Vis_left': [3, 77, 91, 103], 
+                    'Vis_right': [35, 89, 92, 105], 
+                    'FP_left': [4, 21, 27, 29, 71, 76], 
+                    'FP_right': [36, 37, 53, 59, 61, 82, 86, 87], 
+                    'Aud_left': [5, 98], 
+                    'Aud_right': [38, 102], 
+                    'Tpole_left': [6, 64], 
+                    'Tpole_right': [39, 65], 
+                    'PMN_left': [7, 24], 
+                    'PMN_right': [40, 55], 
+                    'PON_left': [8, 26], 
+                    'PON_right': [41, 57], 
+                    'MTL_left': [9, 93], 
+                    'MTL_right': [42, 94], 
+                    'CO_left': [10, 17, 20, 32, 69, 75, 97], 
+                    'CO_right': [43, 48, 51, 63, 101, 107], 
+                    'Sal_left': [11, 23, 31, 66, 95], 
+                    'Sal_right': [44, 54, 67, 99], 
+                    'SMd_left': [12, 13, 68, 78], 
+                    'SMd_right': [45, 80, 88], 
+                    'DAN_left': [14, 15, 25, 70, 72], 
+                    'DAN_right': [46, 47, 56, 83, 85], 
+                    'VAN_left': [19, 22], 
+                    'VAN_right': [50, 52],
+                    'DMN_full': [1, 16, 18, 28, 30, 33, 49, 58, 60, 62, 73, 74, 81, 84], 
+                    'SMl_full': [2, 34, 79, 90, 96, 100, 104, 106], 
+                    'Vis_full': [3, 35, 77, 89, 91, 92, 103, 105], 
+                    'FP_full': [4, 21, 27, 29, 36, 37, 53, 59, 61, 71, 76, 82, 86, 87], 
+                    'Aud_full': [5, 38, 98, 102], 
+                    'Tpole_full': [6, 39, 64, 65], 
+                    'PMN_full': [7, 24, 40, 55], 
+                    'PON_full': [8, 26, 41, 57], 
+                    'MTL_full': [9, 42, 93, 94], 
+                    'CO_full': [10, 17, 20, 32, 43, 48, 51, 63, 69, 75, 97, 101, 107], 
+                    'Sal_full': [11, 23, 31, 44, 54, 66, 67, 95, 99], 
+                    'SMd_full': [12, 13, 45, 68, 78, 80, 88], 
+                    'DAN_full': [14, 15, 25, 46, 47, 56, 70, 72, 83, 85], 
+                    'VAN_full': [19, 22, 50, 52]}
+    print('Defined network labels and their numeric labels.')
+
+    # Convert each network's numeric labels into vertex indices and exclude pMTG vertices if there is any overlap
+    # Overlap should only be a few right pMTG vertices bordering the temporal VAN region
+    network_indices = {}
+    for network, labels in network_labels.items():
+        indices = np.where(np.isin(avg_data, labels))[0]
+        # remove any MTG indices from the network mask
+        indices = list(set(indices) - set(mtg_list))
+        network_indices[network] = indices
+        print(f'Network {network} has {len(indices)} vertices.')
+    print('Network indices computed.')
+
+else:
+    avg_img = nib.load(avg_path_full)
+    avg_data = avg_img.get_fdata().squeeze()
+
+    label_ax = avg_img.header.get_axis(0)
+    brain_ax = avg_img.header.get_axis(1)
+
+    base_labels = {
+        'DMN': 1,
+        'Vis': 2,
+        'FP': 3,
+        'DAN': 5,
+        'SCAN': 6,
+        'VAN': 7,
+        'Sal': 8,
+        'CO': 9,
+        'SMh': 10,
+        'SMm': 11,
+        'Aud': 12,
+        'Tpole': 13,
+        'MTL': 14,
+        'PMN': 15,
+        'PON': 16,
+        'SMf': 17,
+    }
+
+    left_mask = brain_ax.name == 'CIFTI_STRUCTURE_CORTEX_LEFT'
+    right_mask = brain_ax.name == 'CIFTI_STRUCTURE_CORTEX_RIGHT'
+
+    network_indices = {}
+
+    for network, label_id in base_labels.items():
+
+        network_indices[f'{network}_left'] = np.where(
+            (avg_data == label_id) & left_mask
+        )[0]
+
+        network_indices[f'{network}_right'] = np.where(
+            (avg_data == label_id) & right_mask
+        )[0]
+
+        network_indices[f'{network}_full'] = np.where(avg_data == label_id)[0]
+
+    for name in network_indices:
+        network_indices[name] = network_indices[name][
+            ~np.isin(network_indices[name], mtg_list)
+        ]
+
+    # define new network_labels dictionary for the full networks
+    network_labels = {f'{network}_left': [label_id] for network, label_id in base_labels.items()}
+    network_labels.update({f'{network}_right': [label_id] for network, label_id in base_labels.items()})
+    network_labels.update({f'{network}_full': [label_id] for network, label_id in base_labels.items()})
+
+    print('Network indices computed.')
 
 
 # Step 4: Average each network time course, correlate it with every pMTG vertex,
@@ -200,8 +260,12 @@ for subject_path in subject_dirs:
 
 # Step 5: Save results to CSV
 results_df = pd.DataFrame(results)
-results_df.to_csv("/labs/greene-lab/lab_members/emily/mtg/data/pMTG_FC_profiles_midb61_meanFC.csv", index=False)
-print("Saved FC profiles to CSV.")
+if FULL_NETWORKS:
+    results_df.to_csv("/labs/greene-lab/lab_members/emily/mtg/data/pMTG_FC_profiles_fullnetworks_meanFC.csv", index=False)
+    print("Saved FC profiles to CSV (full networks).")
+else:
+    results_df.to_csv("/labs/greene-lab/lab_members/emily/mtg/data/pMTG_FC_profiles_midb61_meanFC.csv", index=False)
+    print("Saved FC profiles to CSV.")
 
 
 
